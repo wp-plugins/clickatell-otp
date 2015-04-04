7b24c7a1c6215656071ef6216b5feb550b199859
chris.brand <chris.brand@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-04 13:58:00 +0000

    Instruct bash scripts to fail if any of the called commands exists with non-zero.
    git-svn-id: https://plugins.svn.wordpress.org/clickatell-otp/trunk@1127734 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bin/deploy-plugin.sh
+++ bin/deploy-plugin.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Deploy the plugin to the remote wordpress repository
 set -x
-git fetch click_php master:click_php
-git merge --squash -s subtree --no-commit click_php/master
+set -e
+git svn rebase
+git svn dcommit
--- bin/subtree-sync.sh
+++ bin/subtree-sync.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # This file is a helper script to keep the subtree folder in sync.
 set -x
+set -e
 git fetch click_php master:click_php
 git merge --squash -s subtree --no-commit click_php/master
